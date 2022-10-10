cdad68188c0f100c9b23a6fee55e569216db13d3
Yunshan-522 <shuiyunhaitang@gmail.com>
2022-10-09 23:22:44 -0400

    change

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -30,10 +30,8 @@ public class Basics {
      * method is run.
      * <p>
      * (Relevant reading: 1.1.2. Defining methods)
-     *
-     * @return
      */
-    public static int main(final String[] args) {
+    public static void main(final String[] args) {
         /* 3. In Python, we could print out code using the print() function.
          *    In Java, we do the same thing by using System.out.println();
          *    In the line below, we print the result of 7 + 5
@@ -47,8 +45,6 @@ public class Basics {
          */
         System.out.println("Hello World!");
 
-
-
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
          *        x = 10
@@ -60,14 +56,12 @@ public class Basics {
          *
          *    (Relevant reading: 1.2. Variables and Types)
          */
-        int my_variable = 100;
 
         /*
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
-
+        int my_variable = 100;
 
         /* Do not remove the line below: if you did task 2 correctly, then
          * you should see the message "my_variable's value is 100" if you run
@@ -90,9 +84,7 @@ public class Basics {
          *
          *    (Relevant reading: 1.8.2. for Loops)
          */
-        for (int i = 0; i <= 10; i--) {
-            System.out.println("Current count: #" + i);
-        }
+
         /*
          * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
          *                the following at each iteration:
@@ -105,95 +97,101 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-        /**
-         *  6. Below is a method named split which returns a String,
-         *    as indicated by the 'String' part of the header. It takes one
-         *    parameter named to_split, whose type is also a String.
-         *
-         *    This block of code above the method is called the doc comment -- this
-         *    is equivalent to docstrings in Python.
-         *
-         *    Below, we provide descriptions of what the parameters are and details
-         *    about what is returned.
-         *
-         * Create a String named 'ret' and get the first letter of
-         * each word inside to_string. to_string is a String containing exactly
-         * 7 words, separated by spaces.
-         *
-         * You may want to use String.split() and String.charAt() methods.
-         * See 1.5.2. APIs for their APIs.
-         *
-         * For example, calling the below method on the string:
-         *    "Idol Long Oolong Vertical Europe University Toyota"
-         * should return "ILOVEUT"
-         *
-         * (Relevant readings: 1.4. Strings and 1.4.4. StringBuilder.
-         *                     1.8.2. for Loops may be useful too.)
-         *
-         * @param to_split   A string containing EXACTLY 7 words, separated by
-         *                   spaces.
-         * @return The first letter of every word in to_split
-         */
-        public static String split (String to_split) {
-            /* TODO (Task 4): Complete this method body.
-             *                The String methods .split and .charAt may be helpful,
-             *                along with the StringBuilder.append
-             *                You may also assume that to_split always has 7 words
-             *                separated by spaces, so no loops are required, though
-             *                you may use them if you wish.
-             */
-            StringBuilder ret = new StringBuilder();
-            for (int i = 0; i < 7; i++){
-                ret.append(to_split.split()[i].charAt(0));
-            }
-
-            // Fill in the rest of the body here
-
-            return ret.toString();
+        for (int a = 10; a >= 0; a--) {
+            System.out.println("Current count: " + a);
         }
 
-        /**
-         * 7. Below is a method that takes in an integer array (similar to a list
-         *    of integers). The method's name is 'oddSum' and it returns an int
-         *    as indicated by the 'int' in the method header.
-         *
-         * Return the sum of all integers at odd indices in the given array.
-         * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
-         * return 0.
-         *
-         * (Relevant readings: 1.6. Arrays and 1.8.2. for Loops)
-         *
-         * @param arr    An array of integers
-         * @return The sum of all integers at odd indices in arr
-         */
-        public static int oddSum (int[] arr){
-            if (arr.length < 1) {
-                int current_sum = 0;
-                for (i = 1; i < arr.length; i += 2) {
-                    current_sum += arr[i];
-                }
-            }else {
-                return 0;
-            }
-            /* TODO (Task 5): Complete this method body using a for-loop.
-             *                You can find the length of an array by using the
-             *                .length attribute (e.g. arr.length)
-             *                You can index into arrays as we do in Python
-             *                (e.g. arr[i] gives you the item at index i).
-             */
-            return current_sum;
-        }
-
-
-        /*
-         * TODO (Task 6): Submit the changes you made on GitHub!
-         *                When you submit it, go to the 'Actions' tab. You should
-         *                be able to see this exercise being autograded!
-         *
-         *                You can click on the ongoing action to see more details.
-         *                If the tests don't pass, look at the results and revise
-         *                accordingly.
-         */
     }
+
+    /**
+     * 6. Below is a method named split which returns a String,
+     * as indicated by the 'String' part of the header. It takes one
+     * parameter named to_split, whose type is also a String.
+     * <p>
+     * This block of code above the method is called the doc comment -- this
+     * is equivalent to docstrings in Python.
+     * <p>
+     * Below, we provide descriptions of what the parameters are and details
+     * about what is returned.
+     * <p>
+     * Create a String named 'ret' and get the first letter of
+     * each word inside to_string. to_string is a String containing exactly
+     * 7 words, separated by spaces.
+     * <p>
+     * You may want to use String.split() and String.charAt() methods.
+     * See 1.5.2. APIs for their APIs.
+     * <p>
+     * For example, calling the below method on the string:
+     * "Idol Long Oolong Vertical Europe University Toyota"
+     * should return "ILOVEUT"
+     * <p>
+     * (Relevant readings: 1.4. Strings and 1.4.4. StringBuilder.
+     * 1.8.2. for Loops may be useful too.)
+     *
+     * @param to_split A string containing EXACTLY 7 words, separated by
+     *                 spaces.
+     * @return The first letter of every word in to_split
+     */
+    public static String split(String to_split) {
+        /* TODO (Task 4): Complete this method body.
+         *                The String methods .split and .charAt may be helpful,
+         *                along with the StringBuilder.append
+         *                You may also assume that to_split always has 7 words
+         *                separated by spaces, so no loops are required, though
+         *                you may use them if you wish.
+         */
+        StringBuilder ret = new StringBuilder();
+
+        // Fill in the rest of the body here
+        String[] aaa = to_split.split(" ");
+        for (String a : aaa) {
+            ret.append(a.charAt(0));
+        }
+
+        return ret.toString();
+    }
+
+    /**
+     * 7. Below is a method that takes in an integer array (similar to a list
+     * of integers). The method's name is 'oddSum' and it returns an int
+     * as indicated by the 'int' in the method header.
+     * <p>
+     * Return the sum of all integers at odd indices in the given array.
+     * If there are no items at odd indices (e.g. a list with 0 or 1 elements),
+     * return 0.
+     * <p>
+     * (Relevant readings: 1.6. Arrays and 1.8.2. for Loops)
+     *
+     * @param arr An array of integers
+     * @return The sum of all integers at odd indices in arr
+     */
+    public static int oddSum(int[] arr) {
+        int current_sum = 0;
+
+        /* TODO (Task 5): Complete this method body using a for-loop.
+         *                You can find the length of an array by using the
+         *                .length attribute (e.g. arr.length)
+         *                You can index into arrays as we do in Python
+         *                (e.g. arr[i] gives you the item at index i).
+         */
+        if (arr.length <= 1) {
+            return 0;
+        }
+        for (int a = 1; a < arr.length; a += 2) {
+            current_sum += arr[a];
+        }
+
+        return current_sum;
+    }
+
+
+    /*
+     * TODO (Task 6): Submit the changes you made on GitHub!
+     *                When you submit it, go to the 'Actions' tab. You should
+     *                be able to see this exercise being autograded!
+     *
+     *                You can click on the ongoing action to see more details.
+     *                If the tests don't pass, look at the results and revise
+     *                accordingly.
+     */
 }
